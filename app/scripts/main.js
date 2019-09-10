782f4d7ee4f1c760483e15a60020cae88edeed7e
Charlie Fox <cfox@100587-mac.local>
2019-09-10 13:47:11 -0600

    bug fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,13 @@
-console.log('\'Allo \'Allo!');
+$('.toggle-info').on( "click", function(e) {
+  e.preventDefault();
+  $(this).text(function(i, text){
+    if ($(this).text === "CLOSE") {
+      return text === "SEE MORE" ? "CLOSE" : "SEE MORE";
+    } else {
+      return text === "CLOSE" ? "SEE MORE" : "CLOSE";
+    }
+  })
+  $(this).prev('.drawer').toggle();
+  $(this).prev('.drawer').removeClass('hidden');
+});
 
